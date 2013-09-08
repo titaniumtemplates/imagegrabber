a728289210a7bd482026ec91cd62d1d1ef757dab
Stavros Kounis <skounis@gmail.com>
2013-09-08 20:31:32 +0300

    Better error handling Image filters

--- Resources/lib/imagegrabber.js
+++ Resources/lib/imagegrabber.js
@@ -2,56 +2,97 @@ Ti.include('htmlparser.js');
 Ti.include('soupselect.js');
 Ti.include('urldescriptor.js');
 
+var MIN_WIDTH = 140;
+//  w:h
+//  4:4 = 1
+//  4:3 = 1.33
+// 16:9 = 1.78 
+var MAX_ASPECT_RATIO = 2.5;
+var MIN_ASPECT_RATIO = 0.6;
+
 exports = {};
 
 exports.grab = function(url, callback){
+		
 	var descriptor = urldescriptor.describe(url);
+	// NOTE: this is not safe. Check the following comments
 	var base_url = descriptor.base_url;
 	
-	var select = soupselect.select;
+	//
+	// Locate base URL
+	// there are cases where the initialy given URL is just a 
+	// redirection to the actual URL. In these cases the base_url
+	// is not safe to be taken by the initial given URL.
+	// eg: http://us.lrd.yahoo.com/_ylc=X3oDMTBia3FyNjc4BHNlY3Rpb24DcnNz/SIG=114duk3t2/**http%3A//www.colonialamerica.com/ 
+	//
+	cb = function(e){
+		Ti.API.info("cb .." + e.toString());
+		Ti.API.info("cb .." + this.location);
+		base_url = this.location;
+		grabDo();
+	};
 	
-	var handler = new htmlparser.DefaultHandler(function(err, dom) {
-		if (err) {
-			alert('Error: ' + err);
-		} else {
-			
-			var img = select(dom, 'body img');
-			var d;
-			var src;
-			var img_urls = new Array();
-			img.forEach(function(img) {
-				d = urldescriptor.describe(img.attribs.src);
-				if (d.base_url == urldescriptor.RELATIVE_URL){
-					src = base_url + '/' + img.attribs.src;
-				} else{
-					src = img.attribs.src;
-				}
-				img_urls.push(src);
-				// alert('src: ' + img.attribs.src);
-			});
-			callback(img_urls);
-		}
-	});
+	APIGetRequest(url, cb, null);
 	
-	var parser = new htmlparser.Parser(handler);
+	function grabDo(){
+		var select = soupselect.select;
+		
+		var handler = new htmlparser.DefaultHandler(function(err, dom) {
+			if (err) {
+				alert('Error: ' + err);
+			} else {
+				
+				var img = select(dom, 'body img');
+				var d;
+				var src;
+				var img_urls = new Array();
+				img.forEach(function(img) {
+					Ti.API.info("img ...: " + img.attribs.src);
+					if (img.attribs.src != undefined){
+						d = urldescriptor.describe(img.attribs.src);
+						if (d!=null){
+							if (d.base_url == urldescriptor.RELATIVE_URL){
+								src = base_url + '/' + img.attribs.src;
+							} else{
+								src = img.attribs.src;
+							}
+							img_urls.push(src);
+							// alert('src: ' + img.attribs.src);
+						}else{
+							Ti.API.warn("Unable to describe URL: " + img.attribs.src);
+						}
+					}else{
+						Ti.API.warn("Img element without src attribute: " + img.raw);
+					}
+				});
+				callback(img_urls);
+			}
+		});
+		
+		var parser = new htmlparser.Parser(handler);
+		
+		APIGetRequest(url, function(e){
+			var status = this.status;
+			if (status == 200) {
+				body = this.responseText;
+				parser.parseComplete(body);
+			}
+		}, function(err){
+			alert('Unknow error from api');
+		});
+	}
 	
-	APIGetRequest(url, function(e){
-		var status = this.status;
-		if (status == 200) {
-			body = this.responseText;
-			parser.parseComplete(body);
-		}
-	}, function(err){
-		alert('Unknow error from api');
-	});
-	
+
+// 	
 };
 
 exports.info = function(image_url){
 	var imageTemp = Ti.UI.createImageView({
 		image : image_url,
+		// height:'auto',
+		// width:'auto'
 		height:'auto',
-		width:'auto'
+		width:10
 	});
 	imageSize = imageTemp.toImage();
 	
@@ -73,6 +114,33 @@ function compare(a, b) {
 };
 
 exports.compare = compare;
+
+// returns a filtered array 
+// of image infos
+function filter(infos){
+	function myfilter(element) {
+		if (element.width <= MIN_WIDTH) {
+			return false;
+		}
+		
+		var ratio = element.width/element.height;
+		
+		if (ratio > MAX_ASPECT_RATIO){
+			return false;
+		}
+		if (ratio < MIN_ASPECT_RATIO){
+			return false;
+		}
+		
+  		return true;
+	}
+	
+	var filtered =  infos.filter(myfilter);
+	
+	return filtered;
+}
+
+exports.filter = filter;
 
 // returns the bigest image 
 // from an array of image infos
--- Resources/ui/handheld/ApplicationWindow.js
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -1,13 +1,17 @@
 Ti.include('/lib/imagegrabber.js');
 
 var test_urls = new Array();
+
+test_urls.push("http://us.lrd.yahoo.com/_ylc=X3oDMTBia3FyNjc4BHNlY3Rpb24DcnNz/SIG=114duk3t2/**http%3A//www.colonialamerica.com/");
+test_urls.push("http://www.collegeonline.org/");
 test_urls.push("http://news.yahoo.com/kerry-sarin-syria-assad-140543565.html");
 test_urls.push("http://sports.yahoo.com/blogs/nfl-shutdown-corner/pam-oliver-suffered-serious-concussion-symptoms-five-days-150415828--nfl.html");
 test_urls.push("http://music.yahoo.com/news/electric-zoo-canceled-two-deaths-during-festival-134023883-rolling-stone.html");
+test_urls.push("http://www.vetstreet.com/our-pet-experts/use-your-spidey-sense-to-keep-pets-away-from-these-arachnids");
 
 // HINT: In order to test several url 
 //       change the index below:
-var url = test_urls[0];
+var url = test_urls[5];
 
 //Application Window Component Constructor
 function ApplicationWindow() {
@@ -27,7 +31,7 @@ function ApplicationWindow() {
 		var descriptor;
 		var infos = [];
 		images.forEach(function(src) {
-			Ti.API.info('');
+			Ti.API.info(' ');
 			Ti.API.info('>>> src ..: ' + src);
 			var i = imagegrabber.info(src);
 			Ti.API.info('> height =' + i.height);
@@ -36,18 +40,28 @@ function ApplicationWindow() {
 			Ti.API.info('> ........: ' + JSON.stringify(i));
 			infos.push(i);
 		});
-		var big = imagegrabber.bigimage(infos);
-		Ti.API.info('');
+		
+		var filtered = imagegrabber.filter(infos);
+		
+		var big = imagegrabber.bigimage(filtered);
+		Ti.API.info(' ');
 		Ti.API.info('>>> big image');
 		Ti.API.info('>   ' + JSON.stringify(big));
 
-		var image = Ti.UI.createImageView({
-	  			image: big.url,
-	  			width: 'auto',
-	  			height: 'auto'
-		});
-		firstView.add(image);
-		
+		if (big != undefined){
+			var image = Ti.UI.createImageView({
+		  			image: big.url,
+		  			width: 'auto',
+		  			height: 'auto'
+			});
+			firstView.add(image);		
+		}else{
+			var label = Titanium.UI.createLabel(
+				{text: 'No image found.'}
+			);
+			firstView.add(label);
+		}
+
 		// var image = Ti.UI.createImageView({
   			// image: big.src,
   			// width: 'auto',
